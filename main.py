f5c5cb5c30cdf5b2cce4fef2712a74452f053a87
PikaKight <tmmarcus73@gmail.com>
2024-01-27 11:32:03 -0500

    added some ui

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
 import streamlit as st
+import pandas as pd
 
 from backend.recommender import recMusics
 
 TITLE = "SongTrx"
+
+songs = pd.read_csv('resources/tcc_ceds_music.csv')
+
+songs = songs[["artist_name", "track_name", "genre"]]
+
+songs["songs"] = songs["track_name"] + "-" + songs["artist_name"]
 
 st.set_page_config(
     page_title=TITLE, page_icon=":bar_chart:", layout="centered"
@@ -13,3 +20,10 @@ st.markdown('##')
 
 with st.sidebar.expander("Age", expanded=True):
     age = st.slider("Select your age:", 0, 120)
+
+with st.sidebar.expander("Songs You Like", expanded=True):
+    songs = st.multiselect(
+        "Select your favorate songs:",
+        options=songs["songs"].unique()
+        )
+    
