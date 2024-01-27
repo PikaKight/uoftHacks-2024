5d24753110dcf7ba5e05d2f23e2770e91bd6d442
PikaKight <tmmarcus73@gmail.com>
2024-01-26 23:53:40 -0500

    got the recommender working

--- backend/recommender.py
+++ backend/recommender.py
@@ -3,3 +3,33 @@ import os
 import re
 
 from datetime import datetime
+from dotenv import load_dotenv
+
+load_dotenv('.env')
+
+co = cohere.Client(os.getenv('COHERE'))
+
+CHILDHOOD = [3, 12]
+
+
+def recMusics(numSongs:int, age: int, songs: list, genres: list) -> list:
+    cYear = datetime.now().year
+    
+    yearRang = f"{cYear - (age - CHILDHOOD[0])} to {cYear - (age - CHILDHOOD[1])}"
+    song = ', '.join(songs)
+    genre = ', '.join(genres)
+    
+    msg = f"What are {numSongs} popular songs from {yearRang} that are similar to {song} and similar genre to {genre}."
+            
+    res = co.chat(message=msg, connectors=[{'id': 'web-search'}], citation_quality='fast')
+
+    print(res.text)
+
+
+if __name__ == "__main__":
+    numSongs = 10
+    age = 21
+    songs = ["Believer - Imagine Dragon", "Pop Star - KDA"]
+    genres = ["Pop", "KPop", "JPop", "MandoPop"]
+
+    recMusics(numSongs, age, songs, genres)
